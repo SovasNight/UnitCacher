9aa3c8607c8a2e9398fa054cf9836497285bf030
SovaNight <nikitos.sleep@gmail.com>
2021-10-06 16:33:24 +0400

    Released Properties Cacher

---
+++ PropertiesCacher/PropertyAccessor.cs
@@ -0,0 +1,22 @@
+﻿namespace PropertiesCacher
+{
+    /// <summary>
+    /// Abstract class define accessors methods for delegated property.
+    /// </summary>
+    public abstract class PropertyAccessor
+    {
+        /// <summary>
+        /// Gets a boxed value from an instance property.
+        /// </summary>
+        /// <param name="instance">The instance which type declares this property.</param>
+        /// <returns>The boxed property value.</returns>
+        public abstract object Get_Value(object instance);
+
+        /// <summary>
+        /// Sets a boxed value to an instance property.
+        /// </summary>
+        /// <param name="instance">The instance which type declares this property.</param>
+        /// <param name="value">The boxed value to be set.</param>
+        public abstract void Set_Value(object instance, object value);
+    }
+}
